Correcting intersection calculation bug and correcting plotting func.

study_hpo.py
@@ -119,28 +119,36 @@ def evaluate_FSBO(hpob_hdlr, keys_to_evaluate):
 
 def plot_rank_graph(n_keys, n_trials):
     # Loading previous outputs
-    gp_performance = load_object("./optimization_results/gp_performance")[:n_keys, :n_trials]
-    rs_performance = load_object("./optimization_results/rs_performance")[:n_keys, :n_trials]
-    de = load_object("./optimization_results/de_performance_32x32_E1000_l0_02_random_start")[:n_keys, :n_trials]
-    dkt = load_object("./optimization_results/dkt_performance_500_003_cosA")[:n_keys, :n_trials]
-    ####
+    rs = load_object("./optimization_results/rs_evaluation")
+    # gp = load_object("./optimization_results/gp_evaluation")
+    # de = load_object("./optimization_results/de_evaluation")
+    dkt = load_object("./optimization_results/intermittent_dkt_evaluation_32x5_500_1")
+
+    keys = get_common_keys([rs, dkt])
+    rs_performance = get_performance_array(rs, keys)[:n_keys, :n_trials]
+    # gp_performance = get_performance_array(gp, keys)[:n_keys, :n_trials]
+    # de_performance = get_performance_array(de, keys)[:n_keys, :n_trials]
+    dkt_performance = get_performance_array(dkt, keys)[:n_keys, :n_trials]
+
+    # de_performance = load_object("./optimization_results/de_performance_32x32_E1000_l0_02_random_start")[:n_keys, :n_trials]
+
     # Creating a rank graph for all above methods
-    performance = np.stack((rs_performance, gp_performance, de, dkt), axis=-1)
+    performance = np.stack((rs_performance, dkt_performance), axis=-1)
     # Since rank data ranks in the increasing order, we need to multiply by -1
     rg = scipy.stats.rankdata(-1 * performance, axis=-1)
     rank_rs = np.mean(rg[:, :, 0], axis=0)
-    rank_gp = np.mean(rg[:, :, 1], axis=0)
-    rank_de = np.mean(rg[:, :, 2], axis=0)
-    rank_dkt = np.mean(rg[:, :, 3], axis=0)
-    plt.figure(101)
+    #rank_gp = np.mean(rg[:, :, 1], axis=0)
+    #rank_de = np.mean(rg[:, :, 2], axis=0)
+    rank_dkt = np.mean(rg[:, :, 1], axis=0)
+    plt.figure(np.random.randint(999999999))
     plt.plot(rank_rs)
-    plt.plot(rank_gp)
-    plt.plot(rank_de)
+    #plt.plot(rank_gp)
+    #plt.plot(rank_de)
     plt.plot(rank_dkt)
     legend = ["RS Rank",
-              "GP Rank",
-              "DE Rank [32,32] ep=1000 lr=0.02 (Rand.)",
-              "DKT Rank [32x4] ft=500 lr=0.03 (CosAnne)"
+              #"GP Rank",
+              #"DE Rank [32,32] ep=1000 lr=0.02 (Rand.)",
+              "DKT Rank [32x5] ft=500 ft_lr=0.1"
               ]
     plt.legend(legend)
     plt.show()
@@ -167,7 +175,7 @@ def get_common_keys(eval_object_list):
     common_keys = set(list(eval_object_list[0].keys()))
     for i in range(1, len(eval_object_list)):
         key_set = set(list(eval_object_list[i].keys()))
-        common_keys.intersection(key_set)
+        common_keys = common_keys.intersection(key_set)
 
     return list(common_keys)
 