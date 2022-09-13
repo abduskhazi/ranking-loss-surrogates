a70b2bd0bfdd52285cd0712d231d704f1b98fc49
Abdus Khazi <abduskhazi@gmail.com>
2022-09-13 12:45:00 +0200

    Making accumulation script more flexible

--- accumulate_eval.py
+++ accumulate_eval.py
@@ -5,6 +5,9 @@ from study_hpo import convert_to_json
 #name = "./Q1_Research/ListWise_transfer/1/results/DE_LIST_32x32x10_E1000_l0_02_OPT"
 name = sys.argv[1]
 dest_name = sys.argv[2]
+exp = 430
+if len(list(sys.argv)) > 3:
+    exp = int(sys.argv[3])
 
 def store_object(obj, obj_name):
     with open(obj_name, "wb") as fp:
@@ -15,7 +18,7 @@ def load_object(obj_name):
         return pickle.load(fp)
 
 performance = []
-for i in range(430):
+for i in range(exp):
     partial = load_object(name + str(i))
     performance += partial
 
