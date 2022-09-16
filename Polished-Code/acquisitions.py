b70f029878e191cd7eeaa0122ff9a9a3e3857719
Abdus Khazi <abduskhazi@gmail.com>
2022-09-16 12:10:59 +0530

    Adding new folder - Polished code for ICLR-2023 conference.

---
+++ Polished-Code/acquisitions.py
@@ -0,0 +1,49 @@
+import numpy as np
+import scipy
+from scipy.stats import norm
+
+def average_rank_deep_set(input, DRE):
+    score_list = []
+    for s in DRE.forward_separate_deep_set(input):
+        score_list += [s.detach().numpy().flatten()]
+
+    # Rank the score list and return the mean rank as the acquisition score.
+    score_list = np.stack(score_list)
+    ranks = scipy.stats.rankdata(score_list, axis=-1)
+    mean_rank = np.mean(ranks, axis=0)
+    return mean_rank
+
+def UCB_rank_deep_set(input, rl_model):
+    score_list = []
+    for sl in rl_model.forward_separate_deep_set(input):
+        score_list += [sl.detach().numpy().flatten()]
+
+    # Rank the score list and return the UCB acquisition score.
+    score_list = np.stack(score_list)
+    ranks = scipy.stats.rankdata(score_list, axis=-1)
+    mean_rank = np.mean(ranks, axis=0)
+    var_rank = np.var(ranks, axis=0)
+    return mean_rank + 0.1 * np.sqrt(var_rank)
+
+def EI_rank_deep_set(input, incumbent, DRE):
+    # Append incumbent to the query input (to calculate its rank w.r.t the current query input).
+    input = (input[0], input[1], torch.cat((input[2], incumbent[None, :]), axis=0))
+
+    score_list = []
+    for s in DRE.forward_separate_deep_set(input):
+        score_list += [s.detach().numpy().flatten()]
+
+    # Rank the score list and calculate the mean and standard deviation of the ranks.
+    score_list = np.stack(score_list)
+    ranks = scipy.stats.rankdata(score_list, axis=-1)
+    mean_rank = np.mean(ranks, axis=0)
+    std_rank = np.sqrt(np.var(ranks, axis=0))
+
+    # Obtain and remove the incumbent from the results.
+    best_y = mean_rank[-1]
+    mean_rank = mean_rank[:-1]
+    std_rank = std_rank[:-1]
+
+    # Return the EI acquisition score.
+    z = (mean_rank - best_y) / (std_rank + 1E-9)
+    return (mean_rank - best_y) * norm.cdf(z) + (std_rank + 1E-9) * norm.pdf(z)
